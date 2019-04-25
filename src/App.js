e3ff4a2478b7e5af5bf92b4c9012cf6c9eef04e3
Jeremy Scala <scalajeremy@gmail.com>
2019-04-25 16:26:02 +0200

    test checkerboard

--- src/App.js
+++ src/App.js
@@ -5,20 +5,106 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="checker-board">
+        
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+    		<div class="light square"></div>
+    		<div class="dark square"></div>
+
+    		<div class="black checker" id="black-checker-1"></div>
+    		<div class="black checker" id="black-checker-2"></div>
+    		<div class="black checker" id="black-checker-3"></div>
+    		<div class="black checker" id="black-checker-4"></div>
+    		<div class="black checker" id="black-checker-5"></div>
+    		<div class="black checker" id="black-checker-6"></div>
+    		<div class="black checker" id="black-checker-7"></div>
+    		<div class="black checker" id="black-checker-8"></div>
+    		<div class="black checker" id="black-checker-9"></div>
+    		<div class="black checker" id="black-checker-10"></div>
+    		<div class="black checker" id="black-checker-11"></div>
+    		<div class="black checker" id="black-checker-12"></div>
+    		<div class="red checker" id="red-checker-1"></div>
+    		<div class="red checker" id="red-checker-2"></div>
+    		<div class="red checker" id="red-checker-3"></div>
+    		<div class="red checker" id="red-checker-4"></div>
+    		<div class="red checker" id="red-checker-5"></div>
+    		<div class="red checker" id="red-checker-6"></div>
+    		<div class="red checker" id="red-checker-7"></div>
+    		<div class="red checker" id="red-checker-8"></div>
+    		<div class="red checker" id="red-checker-9"></div>
+    		<div class="red checker" id="red-checker-10"></div>
+    		<div class="red checker" id="red-checker-11"></div>
+    		<div class="red checker" id="red-checker-12"></div>
+
+    	</div>
     </div>
   );
 }
