2ca36bd3dd80226763d4451da53d4bdd1f3f6599
laralcq <laraloicq@gmail.com>
2019-04-26 16:16:02 +0200

    ajout bouton

--- src/homepage.js
+++ src/homepage.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import button from './assets/button-nofond.png';
 
 export default class Homepage extends Component {
   render() {
     return (
       <div className="main-view">
         <div>Checkers Fantasy</div>
-        <Link to="/play">Go to the main board</Link>
 
         <div id="button">
-        <button type="button"></button>
+          <Link to="/play"><img src={button} alt=""/></Link>
         </div>
       </div>
 
