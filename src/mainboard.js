ae663ecfd130c25b2b73dc8dcc4db2b7ed7e2238
Jeremy Scala <scalajeremy@gmail.com>
2019-04-26 15:59:47 +0200

    pawn position ok, need script for case & pawn

--- src/mainboard.js
+++ src/mainboard.js
@@ -77,11 +77,40 @@ export default class Mainboard extends Component {
       		<div className="light square"></div>
       		<div className="dark square"></div>
 
-          <div className="blue checker" id="blue-checker-1"></div>
-          <div className="red checker" id="red-checker-1"></div>
+          {/* Blue */}
+          <div className="blue checker" style={{marginLeft:'0px'}} id="blue-checker-1"></div>
+          <div className="blue checker" style={{marginLeft:'170px'}} id="blue-checker-1"></div>
+          <div className="blue checker" style={{marginLeft:'340px'}} id="blue-checker-1"></div>
+          <div className="blue checker" style={{marginLeft:'510px'}} id="blue-checker-1"></div>
+
+          <div className="blue checker" style={{marginLeft:'85px', marginTop: '85px'}} id="blue-checker-1"></div>
+          <div className="blue checker" style={{marginLeft:'255px', marginTop: '85px'}} id="blue-checker-1"></div>
+          <div className="blue checker" style={{marginLeft:'425px', marginTop: '85px'}} id="blue-checker-1"></div>
+          <div className="blue checker" style={{marginLeft:'595px', marginTop: '85px'}} id="blue-checker-1"></div>
+
+          <div className="blue checker" style={{marginLeft:'0px', marginTop: '170px'}} id="blue-checker-1"></div>
+          <div className="blue checker" style={{marginLeft:'170px', marginTop: '170px'}} id="blue-checker-1"></div>
+          <div className="blue checker" style={{marginLeft:'340px', marginTop: '170px'}} id="blue-checker-1"></div>
+          <div className="blue checker" style={{marginLeft:'510px', marginTop: '170px'}} id="blue-checker-1"></div>
+
+          {/* Red */}
+          <div className="red checker" style={{marginLeft:'85px', marginTop: '595px'}} id="red-checker-1"></div>
+          <div className="red checker" style={{marginLeft:'255px', marginTop: '595px'}} id="red-checker-1"></div>
+          <div className="red checker" style={{marginLeft:'425px', marginTop: '595px'}} id="red-checker-1"></div>
+          <div className="red checker" style={{marginLeft:'595px', marginTop: '595px'}} id="red-checker-1"></div>
+
+          <div className="red checker" style={{marginLeft:'0px', marginTop: '510px'}} id="red-checker-1"></div>
+          <div className="red checker" style={{marginLeft:'170px', marginTop: '510px'}} id="red-checker-1"></div>
+          <div className="red checker" style={{marginLeft:'340px', marginTop: '510px'}} id="red-checker-1"></div>
+          <div className="red checker" style={{marginLeft:'510px', marginTop: '510px'}} id="red-checker-1"></div>
+
+          <div className="red checker" style={{marginLeft:'85px', marginTop: '425px'}} id="red-checker-1"></div>
+          <div className="red checker" style={{marginLeft:'255px', marginTop: '425px'}} id="red-checker-1"></div>
+          <div className="red checker" style={{marginLeft:'425px', marginTop: '425px'}} id="red-checker-1"></div>
+          <div className="red checker" style={{marginLeft:'595px', marginTop: '425px'}} id="red-checker-1"></div>
+
 
         </div>
-        <div>Je suis un element bootstrap pour test</div>
       </div>
     );
   }
