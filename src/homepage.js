3477b2e65acbaa9beddf53567b5be60b197c8acf
laralcq <laraloicq@gmail.com>
2019-04-26 15:59:01 +0200

    bouton

--- src/homepage.js
+++ src/homepage.js
@@ -4,10 +4,16 @@ import {Link} from 'react-router-dom';
 export default class Homepage extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="main-view">
         <div>Checkers Fantasy</div>
         <Link to="/play">Go to the main board</Link>
+
+        <div id="button">
+        <button type="button"></button>
+        </div>
       </div>
+
+
     );
   }
 }
