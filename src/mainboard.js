9fd4f996e0b7fa788e4616f0e49b44686337667b
Jeremy Scala <scalajeremy@gmail.com>
2019-04-29 11:46:56 +0200

    add logic for moving everywhere

--- src/mainboard.js
+++ src/mainboard.js
@@ -8,6 +8,7 @@ export default class Mainboard extends Component {
 
     this.state = {
       board: {},
+      selected: ''
     }
     console.log(this.state.board)
     const colNames = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
@@ -26,13 +27,13 @@ export default class Mainboard extends Component {
   componentDidMount(){
     const reds = ['A1', 'A3', 'B2', 'C1', 'C3', 'D2', 'E1', 'E3', 'F2', 'G1', 'G3', 'H2'];
     const blues = ['A7', 'B6', 'B8', 'C7', 'D6', 'D8', 'E7', 'F6', 'F8', 'G7', 'H6', 'H8'];
-    reds.forEach(pos => this.addPiece(pos, 'red'));
-    blues.forEach(pos => this.addPiece(pos, 'blue'));
+    reds.forEach(pos => this.changePiece(pos, <div className="red checker"></div>));
+    blues.forEach(pos => this.changePiece(pos, <div className="blue checker"></div>));
   }
 
-  addPiece(id, color){
+  changePiece(id, content){
     let newBoard = this.state.board;
-    newBoard[id].content = (<div className={color + " checker"}></div>)
+    newBoard[id].content = content
     this.setState({board: newBoard});
   }
 
@@ -48,10 +49,16 @@ export default class Mainboard extends Component {
 
   handleEmpty(choice){
     console.log(choice + ' est vide');
+    if(this.state.selected){
+      this.changePiece(choice, this.state.board[this.state.selected].content);
+      this.changePiece(this.state.selected, '');
+    }
   }
 
   handleOccupied(choice){
     console.log('y a quelqu\'un en ' + choice);
+    this.setState({selected: choice});
+
   }
 
 
