a1a348b8e14f1cb8b1552bf7e2c566a3d03d3bb3
Jeremy Scala <scalajeremy@gmail.com>
2019-05-02 15:06:49 +0200

    logic almost done in front

--- src/mainboard.js
+++ src/mainboard.js
@@ -24,7 +24,9 @@ export default class Mainboard extends Component {
           color: (row+col)%2 ? 'light' : 'dark',
           id: colNames[col] + (row+1),
           content: '',
-          pieceColor: ''
+          pieceColor: '',
+          highlighted: false,
+          queen: false
         };
       }
     }
@@ -33,14 +35,15 @@ export default class Mainboard extends Component {
   componentDidMount(){
     const reds = ['A1', 'A3', 'B2', 'C1', 'C3', 'D2', 'E1', 'E3', 'F2', 'G1', 'G3', 'H2'];
     const blues = ['A7', 'B6', 'B8', 'C7', 'D6', 'D8', 'E7', 'F6', 'F8', 'G7', 'H6', 'H8'];
-    reds.forEach(pos => this.changePiece(pos, <div className="red checker"></div>, 'red'));
-    blues.forEach(pos => this.changePiece(pos, <div className="blue checker"></div>, 'blue'));
+    reds.forEach(pos => this.changePiece(pos, <div className="red checker"></div>, 'red', false));
+    blues.forEach(pos => this.changePiece(pos, <div className="blue checker"></div>, 'blue', false));
   }
 
-  changePiece(id, content, color){
+  changePiece(id, content, color, isQueen){
     let newBoard = this.state.board;
     newBoard[id].content = content
     newBoard[id].pieceColor = color;
+    newBoard[id].queen = isQueen;
     this.setState({board: newBoard});
   }
 
@@ -50,38 +53,68 @@ export default class Mainboard extends Component {
      if(this.state.board[choice].content === ''){
        this.handleEmpty(choice)
      }
-     else if(this.state.board[choice].content !== ''){
+     else {
        this.handleOccupied(choice)
      }
   }
 
   handleEmpty(choice){
-    if(this.state.selected){
-      if(this.state.legalMove.indexOf(choice) !== -1){
-        let selected = this.state.board[this.state.selected];
-        let pieceColor = selected.pieceColor;
-        this.changePiece(choice, selected.content, pieceColor);
-        this.changePiece(this.state.selected, '', '');
-        this.setState({legalMove: []});
-      }
-      else{
-        console.log('move pas legal')
-      }
-    }
-  }
+   if(this.state.selected){
 
-  async handleOccupied(choice){
+     let index = this.state.legalMove.indexOf(choice)
+     if(index !== -1){
+       let newBoard = {...this.state.board};
+       this.state.legalMove.forEach(element => {newBoard[element].highlighted = false});
+       this.setState({board: newBoard});
+       let selected = this.state.board[this.state.selected];
+       let pieceColor = selected.pieceColor;
+
+       let row_origin = parseInt(this.state.selected.substring(1));
+       let row_destination = parseInt(choice.substring(1));
+       if(row_destination === 8 && pieceColor === 'red'){
+         this.changePiece(choice, <div className="red-queen checker"></div>, pieceColor, true)
+
+       }
+       else if(row_destination === 1 && pieceColor === 'blue'){
+         this.changePiece(choice, <div className="blue-queen checker"></div>, pieceColor, true)
+
+       }
+       else {
+         this.changePiece(choice, selected.content, pieceColor, selected.queen);
+       }
+       if (Math.abs(row_origin - row_destination) > 1) {
+         let column_origin = this.state.colNames.indexOf(this.state.selected.substring(0, 1));
+         let column_destination = this.state.colNames.indexOf(choice.substring(0, 1));
+
+
+         this.changePiece((this.state.colNames[(column_origin + column_destination) / 2] + ((row_origin + row_destination) / 2)) , '', '', false);
+       }
+       this.changePiece(this.state.selected, '', '', false);
+       this.setState({legalMove: []});
+     }
+     else{
+       console.log('move pas legal')
+     }
+   }
+ }
+
+ async handleOccupied(choice){
     await this.setState({selected: choice});
-    let availableMove = this.possibleMove();
-    await this.setState({legalMove: availableMove})
+    let legalMove = this.possibleMove();
+    this.setState({legalMove: legalMove});
+    let newBoard = {...this.state.board};
+    for(let square in newBoard){
+      newBoard[square].highlighted = legalMove.includes(square);
+    }
 
+    this.setState({board: newBoard});
   }
 
   possibleMove(){
     let selected = this.state.board[this.state.selected];
     let pieceColor = selected.pieceColor;
     let letter = this.state.selected.substring(0,1);
-    let column = this.state.colNames.indexOf(letter) +1;
+    let column = this.state.colNames.indexOf(letter) + 1;
     let row = parseInt(this.state.selected.substring(1));
     let legalMove = [];
 
@@ -90,51 +123,66 @@ export default class Mainboard extends Component {
     let moveLowerLeft = this.state.colNames[column - 2] + (row + 1);
     let moveLowerRight = this.state.colNames[column] + (row + 1);
 
-    if((column - 2 >= 0) && (row < 8) && (this.state.board[moveLowerLeft].content === '') && (pieceColor === 'red')){
-      legalMove.push(moveLowerLeft);
+    if(((column - 2 >= 0) && (row < 8) && (this.state.board[moveLowerLeft].content === '' ) && (pieceColor === 'red' || selected.queen)) || ((pieceColor === 'red' || selected.queen) && (column - 3 >= 0) && (row < 7) && (this.state.board[moveLowerLeft].pieceColor !== pieceColor) &&   (this.state.board[(this.state.colNames[column - 3] + (row + 2))].content === ''))){
+      if(this.state.board[moveLowerLeft].content === '')
+        legalMove.push(moveLowerLeft);
+
+      else
+        legalMove.push((this.state.colNames[column - 3] + (row + 2)));
     }
 
-    if((column < 8) && (row < 8) && (this.state.board[moveLowerRight].content === '') && (pieceColor === 'red')){
-      legalMove.push(moveLowerRight);
+    if(((column < 8) && (row < 8) && (this.state.board[moveLowerRight].content === '') && (pieceColor === 'red' || selected.queen)) || ((pieceColor === 'red' || selected.queen) && (column < 7) && (row < 7) && (this.state.board[moveLowerRight].pieceColor !== pieceColor) && (this.state.board[(this.state.colNames[column + 1] + (row + 2))].content === ''))){
+      if(this.state.board[moveLowerRight].content === '')
+        legalMove.push(moveLowerRight);
+
+      else
+        legalMove.push((this.state.colNames[column + 1] + (row + 2)));
     }
 
-    if((column - 2 >= 0) && (row >= 2) && (this.state.board[moveUpperLeft].content === '') && (pieceColor === 'blue')){
-      legalMove.push(moveUpperLeft);
+    if(((column - 2 >= 0) && (row >= 2) && (this.state.board[moveUpperLeft].content === '') && (pieceColor === 'blue' || selected.queen)) || ((pieceColor === 'blue' || selected.queen) && (column - 3 >= 0) && (row > 2) && (this.state.board[moveUpperLeft].pieceColor !== pieceColor) && (this.state.board[(this.state.colNames[column - 3] + (row - 2))].content === ''))){
+      if(this.state.board[moveUpperLeft].content === '')
+        legalMove.push(moveUpperLeft);
+
+      else
+        legalMove.push((this.state.colNames[column - 3] + (row - 2)));
     }
 
-    if((column < 8) && (row >= 2) && (this.state.board[moveUpperRight].content === '') && (pieceColor === 'blue')){
-      legalMove.push(moveUpperRight);
+    if(((column < 8) && (row >= 2) && (this.state.board[moveUpperRight].content === '') && (pieceColor === 'blue' || selected.queen)) || ((pieceColor === 'blue' || selected.queen) && (column < 7) && (row > 2) && (this.state.board[moveUpperRight].pieceColor !== pieceColor) && (this.state.board[(this.state.colNames[column + 1] + (row - 2))].content === ''))){
+      if(this.state.board[moveUpperRight].content === '')
+        legalMove.push(moveUpperRight);
+
+      else
+        legalMove.push((this.state.colNames[column + 1] + (row - 2)));
     }
     return legalMove;
-}
+  }
 
   render() {
     return (
-      <div className="main-view">
-        <div className="game-elements">
-          <div className="top-elements">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-2 align-self-start content-ui" id="perso-left">
               <img src={avatar_red} alt="avatar_red" className="first-pers" />
               <img src={heart} alt ="heart" className="heart" />
               <img src={star} alt ="star" className="star" />
           </div>
-
+        <div className="col align-self-center" id="mainboard">
+          <div id="checker-board">
+            {Object.keys(this.state.board).map(key => {
+              let square = this.state.board[key];
+              return (
+                <div className={`square ${square.color}${square.highlighted ? ' highlighted' : ''}`} id={square.id} key={square.id} onClick={this.handleClick.bind(this)}>                {square.content}
+              </div>
+            )})}
+          </div>
         </div>
-        <div id="checker-board">
-          {Object.keys(this.state.board).map(key => {
-            let square = this.state.board[key];
-            return (
-            <div className={'square ' + square.color} id={square.id} key={square.id} onClick={this.handleClick.bind(this)}>
-              {square.content}
-            </div>
-
-          )})}
-        </div>
-        <div className="top-elements">
+        <div className="col-2 align-self-end">
             <img src={avatar_blue} alt="avatar_blue" className="second-pers" />
             <img src={heart} alt ="heart" className="heart" />
             <img src={star} alt ="star" className="star" />
         </div>
       </div>
+    </div>
     );
   }
 }
